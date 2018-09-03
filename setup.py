35e22b6a066682d2ce47f2c8b82caf487e0233d5
Gram <master_fess@mail.ru>
2018-09-03 13:23:40 +0300

    release new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='install-requires',
-    version='0.2.1',
+    version='0.3.0',
     description='Convert requirements between formats',
     long_description=long_description,
     long_description_content_type='text/markdown',
